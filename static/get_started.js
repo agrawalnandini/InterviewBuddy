a22164d4f66ab4d988171ab78add509c9f052ca4
ananya <aa4819@columbia.edu>
2022-10-27 04:14:36 -0400

    Changinf function name for relevancy

--- static/get_started.js
+++ static/get_started.js
@@ -1,12 +1,12 @@
 $(document).ready(function(){
-    load_categories_in_dropdown(options = fields, dropdown_id = "#field-options-select")
-    load_categories_in_dropdown(options = topics, dropdown_id = "#topic-options-select")
+    render_categories_in_dropdown(options = fields, dropdown_id = "#field-options-select")
+    render_categories_in_dropdown(options = topics, dropdown_id = "#topic-options-select")
     $("#generate-questions-btn").click(function(){
         generate_questions_btn_click()
     })
 })
 
-function load_categories_in_dropdown(options, dropdown_id){
+function render_categories_in_dropdown(options, dropdown_id){
     $.each(options, function(index, value){
         $("<option value = " + index + ">" + value + "</option>").appendTo(dropdown_id)
     })
